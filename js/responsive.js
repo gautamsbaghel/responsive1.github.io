72810addc7489db330d05e16291259bd6047a61a
gautamsbaghel <baghelgautam2001@gmail.com>
2021-01-24 12:36:09 +0530

    Update responsive.js

--- js/responsive.js
+++ js/responsive.js
@@ -4,7 +4,14 @@ navList = document.querySelector('.navList')
 rightNav = document.querySelector('.rightNav')
 
 
-burger.addEventListener('click', () => {
+
+burger.addEventListener('mouseover', () => {
+    rightNav.classList.toggle('v-class-resp');
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+})
+
+navbar.addEventListener('click', () => {
     rightNav.classList.toggle('v-class-resp');
     navList.classList.toggle('v-class-resp');
     navbar.classList.toggle('h-nav-resp');
