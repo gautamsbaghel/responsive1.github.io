f12dfe1447f6a6f2fd28f912828d196adc830dca
gautamsbaghel <baghelgautam2001@gmail.com>
2021-01-24 12:48:12 +0530

    Update responsive.js

--- js/responsive.js
+++ js/responsive.js
@@ -2,16 +2,36 @@ burger = document.querySelector('.burger')
 navbar = document.querySelector('.navbar')
 navList = document.querySelector('.navList')
 rightNav = document.querySelector('.rightNav')
+button = document.querySelector('.btn-sm')
 
 
 
-burger.addEventListener('mouseover', () => {
+burger.addEventListener('click', () => {
     rightNav.classList.toggle('v-class-resp');
     navList.classList.toggle('v-class-resp');
     navbar.classList.toggle('h-nav-resp');
 })
 
-navbar.addEventListener('click', () => {
+
+navList.addEventListener('click', () => {
+    rightNav.classList.toggle('v-class-resp');
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+})
+
+button.addEventListener('click', () => {
+    rightNav.classList.toggle('v-class-resp');
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+})
+
+navList.addEventListener('tap', () => {
+    rightNav.classList.toggle('v-class-resp');
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+})
+
+button.addEventListener('tap', () => {
     rightNav.classList.toggle('v-class-resp');
     navList.classList.toggle('v-class-resp');
     navbar.classList.toggle('h-nav-resp');
